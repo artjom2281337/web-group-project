bc9bc856e25e94ff23b9eafe5d3fd5ccb9ce8f6c
Artjom Smorgulenko <artjomsmorgulenko@gmail.com>
2026-03-29 18:09:50 +0300

    comments to index.js, reservation.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+// assigning constant variables with api url and html elements
 const QUOTE_SERVER_URL = "https://food-quote-api.vercel.app/api/foodquote"
 const QUOTE_ELEMENT = document.getElementById("food-quote")
 const QUOTE_AUTHOR_ELEMENT_START = '<br><span id="food-quote-author">'
 const QUOTE_AUTHOR_ELEMENT_END = '</span>'
-
+// fetches with api url, receives json data with quote and returns the quote with author to the page
 fetch(QUOTE_SERVER_URL).then(response => {
     response.json().then(body => {
         QUOTE_ELEMENT.innerHTML = `${body.quote}${QUOTE_AUTHOR_ELEMENT_START}${body.author}${QUOTE_AUTHOR_ELEMENT_END}`
--- reservation.js
+++ reservation.js
@@ -1,3 +1,4 @@
+// class with constructor with person details for reservation
 class Person {
     constructor(name, email, phone, date, time, guests) {
         this.name = name;
@@ -8,31 +9,41 @@ class Person {
         this.guests = guests;
     }
 }
+// available table times for reservation
 tableTimes = ["18:00", "19:00", "20:00", "21:00"];
+// function returnTimes() - iterates over the available times and returns the table times
 function returnTimes() {
+    // if values are empty, it alerts the user that user needs to fill all fields
     if (document.getElementById("name").value == "" || document.getElementById("email").value == "" || document.getElementById("phone").value == "" || document.getElementById("date").value == "" || document.getElementById("guests").value == "") {
         alert("Please fill all the fields!");
         return;
     }
+    // available times are appearing
     document.getElementById("table-h2").innerHTML = "Available Times";
+    // times needs to be dissapeared before actual loop iteration in case if user can see the times, so times will not repeating
     document.getElementById("table").innerHTML = "";
+    // for loop to iterate over table times
     for (let i = 0; i < tableTimes.length; i++) {
+        // returns button, that uses the time parameter for next function
         document.getElementById("table").innerHTML += `<button onclick="reserveTable('${tableTimes[i]}')">${tableTimes[i]}</button>`;
     }
 }
+// function reserveTable() with time parameter assigns input values to variables, checks if there's an input data, creates newPerson object with input values, returns input data of user and makes input data dissapeared after user can see own input values
 function reserveTable(time) {
+    // assigning input values to variables
     name = document.getElementById("name").value;
     email = document.getElementById("email").value;
     phone = document.getElementById("phone").value;
     date = document.getElementById("date").value;
     guests = document.getElementById("guests").value;
-
+    // checks if values are empty, if they are empty, user will be alerted that user need to input own data
     if (name == "" || email == "" || phone == "" || date == "" || guests == "") {
         alert("Please fill all the fields!");
         return;
     }
-
+    // creating newPerson object with input variables
     let newPerson = new Person(name, email, phone, date, time, guests);
+    // returns the confirmation of reservation, user can see own input data
     document.getElementById("details").innerHTML = `<h3>Table Reservated!</h3><br>
                                                     <p>Name: ${newPerson.name}</p><br>
                                                     <p>Email: ${newPerson.email}</p><br>
@@ -40,7 +51,7 @@ function reserveTable(time) {
                                                     <p>Date: ${newPerson.date}</p><br>
                                                     <p>Time: ${newPerson.time}</p><br>
                                                     <p>Guests: ${newPerson.guests}</p><br>`;
-    
+    // cleans the input data after user can see own data
     document.getElementById("name").value = "";                                                
     document.getElementById("email").value = "";                                                
     document.getElementById("phone").value = "";                                                
